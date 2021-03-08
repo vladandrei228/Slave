7203e23e5634b94217dc51499e7d64498c9e768c
Vlad Andrei Gruia <vlad0765@edu.easj.dk>
2021-03-08 15:28:35 +0200

    Add project files.

---
+++ Slave/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Slave
+{
+   
+       class Program
+        {
+            static void Main(string[] args)
+            {
+                TcpClient clientSocket = new TcpClient("localhost", 6789);
+
+                Stream ns = clientSocket.GetStream(); //provides a NetworkStream
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+                sw.AutoFlush = true; // enable automatic flushing
+
+                //request the server for a chunk/dictionary/passwords
+                sw.WriteLine("password");
+
+                Console.ReadKey();
+
+                ns.Close();
+                clientSocket.Close();
+
+            }
+        }
+
+    
+}
---
+++ Slave/UserInfoClearText.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slave
+{
+        /// <summary>
+        /// Username + password in clear text (that is human readable)
+        /// </summary>
+        class UserInfoClearText
+        {
+            public String UserName { get; set; }
+            public String Password { get; set; }
+
+            public UserInfoClearText(string username, string password)
+            {
+                if (username == null)
+                {
+                    throw new ArgumentNullException("username");
+                }
+                if (password == null)
+                {
+                    throw new ArgumentNullException("password");
+                }
+                UserName = username;
+                Password = password;
+            }
+
+            public override string ToString()
+            {
+                return UserName + ": " + Password;
+            }
+        }
+ 
+}
